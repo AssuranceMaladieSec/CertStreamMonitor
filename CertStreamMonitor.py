f4684cd6d05b1a63b5c01b5ad29295f0b923aee5
thdamon <thdamon@gmail.com>
2018-01-31 12:08:15 +0100

    Organization's name modif.

--- CertStreamMonitor.py
+++ CertStreamMonitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2018 Cnam 
+# Copyright (c) 2018 Caisse nationale d'Assurance Maladie
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
